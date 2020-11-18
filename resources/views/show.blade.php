d2666554b5e164bb04ce15967379e44bfe77be1f
Tochukwu <ugwukelvintochukwu@gmail.com>
2020-11-18 19:49:14 +0100

    creating image section and adding image to image section

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -113,4 +113,45 @@
     </div>
 
 
+    <div class="movie-images border-b border-gray-800">
+        <div class="container mx-auto px-4 py-16">
+            <h2 class="text-4xl font-semibold">Images</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-8">
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/image1.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-250">
+                    </a>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/image2.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-250">
+                    </a>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/image3.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-250">
+                    </a>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/image4.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-250">
+                    </a>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/image5.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-250">
+                    </a>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/image6.jpg" alt="parasite" class="hover:opacity-75 transition ease-in-out duration-250">
+                    </a>
+                </div>
+
+            </div>
+
+        </div>
+    </div>
+
+
 @endsection
